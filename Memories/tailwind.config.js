0e436eea99a7e23dbebb6b82a2e618dc0b0bcc9f
injili <nyarekigospel@gmail.com>
2025-03-27 14:40:03 +0300

    the progress

--- Memories/tailwind.config.js
+++ Memories/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -8,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
