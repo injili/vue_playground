c2f55c39b13ffff620f7f78c52caf82b993b291f
injili <nyarekigospel@gmail.com>
2025-03-28 13:05:16 +0300

    the progress

---
+++ Memories/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Nav from "./components/Navigation.vue";
+import Content from "./components/Content.vue";
+</script>
+
+<template>
+  <header>
+    <Nav />
+  </header>
+
+  <main>
+    <Content />
+  </main>
+</template>
