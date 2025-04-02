544a74bdfcd355f66a2739e2b59bc3393028a004
injili <nyarekigospel@gmail.com>
2025-04-02 12:02:45 +0300

    the progress

--- Memories/src/components/Content.vue
+++ Memories/src/components/Content.vue
@@ -1,97 +1,116 @@
 <template>
-  <div class="container bg-gray-200 mx-auto w-full h-full">
+  <div class="container bg-white mx-auto w-full h-full rounded-lg">
     <div class="relative wrap overflow-hidden p-10 h-full">
       <div
-        class="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
+        class="border absolute border-opacity-20 border-gray-700 h-full border"
         style="left: 50%"
       ></div>
       <!-- right timeline -->
-      <div class="mb-8 flex justify-between items-center w-full right-timeline">
-        <div class="order-1 w-5/12"></div>
+      <div class="flex justify-between items-center w-full">
+        <div class="order-1 w-6/12"></div>
         <div
-          class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full"
+          class="z-20 flex items-center justify-center order-1 text-black mx-1 sm:mx-2 md:mx-3 lg:mx-4 xl:mx-5"
         >
-          <h1 class="mx-auto font-semibold text-lg text-white">1</h1>
-        </div>
-        <div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-          <h3 class="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-          <p
-            class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
+          <h1
+            class="font-semibold text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl"
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
+            01.04.24
+          </h1>
+        </div>
+        <div
+          class="order-1 bg-gray-400 rounded-lg shadow-xl w-4/12 overflow-hidden aspect-square"
+        >
+          <img
+            :src="images.img1"
+            alt="this image"
+            class="w-full h-full object-cover object-center"
+          />
         </div>
       </div>
 
       <!-- left timeline -->
-      <div
-        class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline"
-      >
-        <div class="order-1 w-5/12"></div>
+      <div class="flex justify-between flex-row-reverse items-center w-full">
+        <div class="order-1 w-6/12"></div>
         <div
-          class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full"
+          class="z-20 flex items-center order-1 text-black mx-1 sm:mx-2 md:mx-3 lg:mx-4 xl:mx-5"
         >
-          <h1 class="mx-auto text-white font-semibold text-lg">2</h1>
-        </div>
-        <div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-          <h3 class="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-          <p
-            class="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
+          <h1
+            class="mx-auto font-semibold text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl"
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
+            11.06.24
+          </h1>
+        </div>
+        <div
+          class="order-1 bg-gray-400 rounded-lg shadow-xl w-4/12 overflow-hidden aspect-square"
+        >
+          <img
+            :src="images.img2"
+            alt="this image"
+            class="w-full h-full object-cover object-center"
+          />
         </div>
       </div>
 
       <!-- right timeline -->
-      <div class="mb-8 flex justify-between items-center w-full right-timeline">
-        <div class="order-1 w-5/12"></div>
+      <div class="flex justify-between items-center w-full">
+        <div class="order-1 w-6/12"></div>
         <div
-          class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full"
+          class="z-20 flex items-center justify-center order-1 text-black mx-1 sm:mx-2 md:mx-3 lg:mx-4 xl:mx-5"
         >
-          <h1 class="mx-auto font-semibold text-lg text-white">3</h1>
-        </div>
-        <div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-          <h3 class="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-          <p
-            class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
+          <h1
+            class="font-semibold text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl"
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
+            30.07.24
+          </h1>
+        </div>
+        <div
+          class="order-1 bg-gray-400 rounded-lg shadow-xl w-4/12 overflow-hidden aspect-square"
+        >
+          <img
+            :src="images.img3"
+            alt="this image"
+            class="w-full h-full object-cover object-center"
+          />
         </div>
       </div>
 
       <!-- left timeline -->
-      <div
-        class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline"
-      >
-        <div class="order-1 w-5/12"></div>
+      <div class="flex justify-between flex-row-reverse items-center w-full">
+        <div class="order-1 w-6/12"></div>
         <div
-          class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full"
+          class="z-20 flex items-center order-1 text-black mx-1 sm:mx-2 md:mx-3 lg:mx-4 xl:mx-5"
         >
-          <h1 class="mx-auto text-white font-semibold text-lg">4</h1>
-        </div>
-        <div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-          <h3 class="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-          <p
-            class="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
+          <h1
+            class="mx-auto font-semibold text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl"
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
+            13.08.24
+          </h1>
+        </div>
+        <div
+          class="order-1 bg-gray-400 rounded-lg shadow-xl w-4/12 overflow-hidden aspect-square"
+        >
+          <img
+            :src="images.img4"
+            alt="this image"
+            class="w-full h-full object-cover object-center"
+          />
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import img1 from "../assets/images/img1.jpeg";
+import img2 from "../assets/images/img2.jpeg";
+import img3 from "../assets/images/img3.jpg";
+import img4 from "../assets/images/img4.jpg";
+
+export default {
+  data() {
+    return {
+      images: { img1, img2, img3, img4 },
+    };
+  },
+};
+</script>
